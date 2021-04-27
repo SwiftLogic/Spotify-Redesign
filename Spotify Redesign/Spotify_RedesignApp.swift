709ac016dbde012a1fb0cf5cb9ace9e2841599a8
Samuel Uyigue <Samuel.uyigue@gmail.com>
2021-04-27 14:14:28 -0500

    Initial Commit

---
+++ Spotify Redesign/Spotify_RedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_RedesignApp.swift
+//  Spotify Redesign
+//
+//  Created by Osaretin Uyigue on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_RedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
